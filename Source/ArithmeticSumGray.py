1db7339d9bfe68ccd82d811580037b8459b182eb
Damian Ubowski <sverda300@gmail.com>
2020-04-23 23:09:53 +0200

    Ex 2.2 done 2

--- Source/ArithmeticSumGray.py
+++ Source/ArithmeticSumGray.py
@@ -40,7 +40,7 @@ class ArithmeticSumGray(object):
         img = Image.fromarray(result, mode='L')
         img.save('Resources/result/sum-gray-const-{}-norm.png'.format(constValue))
         
-    # Ex2.2
+    # Ex2.1
     def sumImages(self):
         print('sum gray image {} with image {}'.format(self.firstDecoder.name, self.secondDecoder.name))
         unification = Unification(self.firstDecoder.name, self.secondDecoder.name, 'L')
@@ -72,3 +72,58 @@ class ArithmeticSumGray(object):
         
         img = Image.fromarray(result, mode='L')
         img.save('Resources/result/sum-gray-images-norm.png')
+
+    # Ex2.2
+    def multiplyWithConst(self, constValue):
+        print('multiply gray image {} with const {}'.format(self.firstDecoder.name, constValue))
+        height, width = self.firstDecoder.height, self.firstDecoder.width
+        image = self.firstDecoder.getPixels()
+        
+        maxSum = float(numpy.amax(numpy.add(image.astype(numpy.uint32), constValue)))
+        maxValue = float(numpy.iinfo(image.dtype).max)
+        
+        result = numpy.ones((height, width), numpy.uint8)
+        for h in range(height):
+            for w in range(width):
+                pom = (image[h, w] * constValue) / maxValue
+                result[h, w] = numpy.ceil(pom)
+
+        img = Image.fromarray(result, mode='L')
+        img.save('Resources/result/multiply-gray-const-{}.png'.format(constValue))
+
+        maxValue = numpy.iinfo(image.dtype).max
+        fmin = numpy.amin(result)
+        fmax = numpy.amax(result)
+        result = result.astype(numpy.float32)
+        result = maxValue * ((result - fmin) / (fmax - fmin))
+        result = result.astype(numpy.uint8)
+        
+        img = Image.fromarray(result, mode='L')
+        img.save('Resources/result/multiply-gray-const-{}-norm.png'.format(constValue))
+
+    # Ex2.2
+    def multiplyImages(self):
+        print('multiply gray image {} with image {}'.format(self.firstDecoder.name, self.secondDecoder.name))
+        unification = Unification(self.firstDecoder.name, self.secondDecoder.name, 'L')
+        firstImage, secondImage = unification.grayUnification()
+        width, height = firstImage.shape[0], firstImage.shape[1]
+        
+        maxValue = float(numpy.iinfo(firstImage.dtype).max)
+        result = numpy.ones((height, width), numpy.uint8)
+        for h in range(height):
+            for w in range(width):
+                pom = (int(firstImage[h, w]) * int(secondImage[h, w])) / maxValue
+                result[h, w] = numpy.ceil(pom)
+
+        img = Image.fromarray(result, mode='L')
+        img.save('Resources/result/multiply-gray-images.png')
+
+        maxValue = numpy.iinfo(firstImage.dtype).max
+        fmin = numpy.amin(result)
+        fmax = numpy.amax(result)
+        result = result.astype(numpy.float32)
+        result = maxValue * ((result - fmin) / (fmax - fmin))
+        result = result.astype(numpy.uint8)
+        
+        img = Image.fromarray(result, mode='L')
+        img.save('Resources/result/multiply-gray-images-norm.png')
